fc297e53c7a89b0b96c6c1750bc46cbe0d8f85c2
Steven Degutis <steven.degutis@gmail.com>
2012-07-18 23:41:47 -0500

    Shortening code a little with setUp/tearDown

--- SpecKitTests/TestIntExpectation.m
+++ SpecKitTests/TestIntExpectation.m
@@ -6,37 +6,42 @@
 
 #import "SenTestCase+ReportingHelper.h"
 
-@interface TestIntExpectation : SenTestCase
+@interface TestIntExpectation : SenTestCase {
+  SKFakeFailureReporter *reporter;
+}
 @end
 @implementation TestIntExpectation
 
+- (void) setUp {
+  reporter = [[SKFakeFailureReporter alloc] init];
+}
+
+- (void) tearDown {
+  [reporter release];
+}
+
 - (void) testToBeFail {
-  SKFakeFailureReporter *reporter = [[[SKFakeFailureReporter alloc] init] autorelease];
   [[SKIntExpectation expectationFunctionInFile:"file11" line:22 failureReporter:reporter](2) toBe: 3];
   SKHelperExpectReport(@"file11", 22, @"Expected 3, got 2");
 }
 
 - (void) testToBeNegativeFail {
-  SKFakeFailureReporter *reporter = [[[SKFakeFailureReporter alloc] init] autorelease];
   [[SKIntExpectation expectationFunctionInFile:"file1" line:2 failureReporter:reporter](-1) toBe: 1];
   SKHelperExpectReport(@"file1", 2, @"Expected 1, got -1");
 }
 
 - (void) testToBePass {
-  SKFakeFailureReporter *reporter = [[[SKFakeFailureReporter alloc] init] autorelease];
   SKIntExpectation *exp = [SKIntExpectation expectationFunctionInFile:"file1" line:2 failureReporter:reporter](1);
   [exp toBe: 1];
   SKHelperExpectNoReport();
 }
 
 - (void) testToBeTrueFail {
-  SKFakeFailureReporter *reporter = [[[SKFakeFailureReporter alloc] init] autorelease];
   [[SKIntExpectation expectationFunctionInFile:"file11" line:22 failureReporter:reporter](NO) toBeTrue];
   SKHelperExpectReport(@"file11", 22, @"Expected true, got false");
 }
 
 - (void) testToBeTruePass {
-  SKFakeFailureReporter *reporter = [[[SKFakeFailureReporter alloc] init] autorelease];
   [[SKIntExpectation expectationFunctionInFile:"file1" line:2 failureReporter:reporter](YES) toBeTrue];
   SKHelperExpectNoReport();
 }
--- SpecKitTests/TestObjectExpectation.m
+++ SpecKitTests/TestObjectExpectation.m
@@ -6,18 +6,26 @@
 #import "SKObjectExpectation.h"
 #import "SKFakeFailureReporter.h"
 
-@interface TestObjectExpectation : SenTestCase
+@interface TestObjectExpectation : SenTestCase {
+  SKFakeFailureReporter *reporter;
+}
 @end
 @implementation TestObjectExpectation
 
+- (void) setUp {
+  reporter = [[SKFakeFailureReporter alloc] init];
+}
+
+- (void) tearDown {
+  [reporter release];
+}
+
 - (void) testToBeEqualToPass {
-  SKFakeFailureReporter *reporter = [[[SKFakeFailureReporter alloc] init] autorelease];
   [[SKObjectExpectation expectationFunctionInFile:"file1" line:2 failureReporter:reporter](@"a") toBeEqualTo: @"a"];
   SKHelperExpectNoReport();
 }
 
 - (void) testToBeEqualToFail {
-  SKFakeFailureReporter *reporter = [[[SKFakeFailureReporter alloc] init] autorelease];
   [[SKObjectExpectation expectationFunctionInFile:"file11" line:22 failureReporter:reporter](@"a") toBeEqualTo: @"b"];
   SKHelperExpectReport(@"file11", 22, @"Expected b, got a");
 }
@@ -25,7 +33,6 @@
 - (void) testToBePass {
   id a = [[[NSObject alloc] init] autorelease];
   
-  SKFakeFailureReporter *reporter = [[[SKFakeFailureReporter alloc] init] autorelease];
   [[SKObjectExpectation expectationFunctionInFile:"file1" line:2 failureReporter:reporter](a) toBe: a];
   SKHelperExpectNoReport();
 }
@@ -34,31 +41,26 @@
   id a = [NSMutableString stringWithString:@"a"];
   id b = [NSMutableString stringWithString:@"b"];
   
-  SKFakeFailureReporter *reporter = [[[SKFakeFailureReporter alloc] init] autorelease];
   [[SKObjectExpectation expectationFunctionInFile:"file11" line:22 failureReporter:reporter](a) toBe: b];
   SKHelperExpectReport(@"file11", 22, @"Expected b, got a");
 }
 
 - (void) testToExistPass {
-  SKFakeFailureReporter *reporter = [[[SKFakeFailureReporter alloc] init] autorelease];
   [[SKObjectExpectation expectationFunctionInFile:"file11" line:22 failureReporter:reporter](@"a") toExist];
   SKHelperExpectNoReport();
 }
 
 - (void) testToExistFail {
-  SKFakeFailureReporter *reporter = [[[SKFakeFailureReporter alloc] init] autorelease];
   [[SKObjectExpectation expectationFunctionInFile:"file11" line:22 failureReporter:reporter](nil) toExist];
   SKHelperExpectReport(@"file11", 22, @"Expected nil, but isn't");
 }
 
 - (void) testToBeNilPass {
-  SKFakeFailureReporter *reporter = [[[SKFakeFailureReporter alloc] init] autorelease];
   [[SKObjectExpectation expectationFunctionInFile:"file11" line:22 failureReporter:reporter](nil) toBeNil];
   SKHelperExpectNoReport();
 }
 
 - (void) testToBeNilFail {
-  SKFakeFailureReporter *reporter = [[[SKFakeFailureReporter alloc] init] autorelease];
   [[SKObjectExpectation expectationFunctionInFile:"file11" line:22 failureReporter:reporter](@"a") toBeNil];
   SKHelperExpectReport(@"file11", 22, @"Expected nil, got a");
 }

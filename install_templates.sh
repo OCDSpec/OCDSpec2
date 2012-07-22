8e64cde000981c83369ecf4094cea1f29c69d240
Steven Degutis <steven.degutis@gmail.com>
2012-07-22 00:22:31 -0500

    Adding Mac target template.

--- install_templates.sh
+++ install_templates.sh
@@ -1,6 +1,13 @@
 #!/usr/bin/env bash
 
+echo "Copying file template..."
 dest="$HOME/Library/Developer/Xcode/Templates/SpecKit-Templates/File Templates/SpecKit/Spec.xctemplate"
 mkdir -p "$dest"
 cp Spec.xctemplate/* "$dest"
+echo "Done."
+
+echo "Copying target template..."
+dest="$HOME/Library/Developer/Xcode/Templates/SpecKit-Templates/Project Templates/SpecKit/Mac Spec Runner.xctemplate"
+mkdir -p "$dest"
+cp "Mac Spec Runner.xctemplate"/* "$dest"
 echo "Done."

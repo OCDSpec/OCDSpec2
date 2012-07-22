1dff3f1e572d883454adb411a3cf06e8e59a16c2
Steven Degutis <steven.degutis@gmail.com>
2012-07-22 01:32:41 -0500

    Moving template stuff around, and making spec file template work for iOS.

--- install_templates.sh
+++ install_templates.sh
@@ -1,19 +1,6 @@
 #!/usr/bin/env bash
 
-echo "Copying file template..."
-dest="$HOME/Library/Developer/Xcode/Templates/SpecKit-Templates/File Templates/SpecKit/Spec.xctemplate"
-mkdir -p "$dest"
-cp "Templates/Spec.xctemplate/"* "$dest"
-echo "Done."
-
-echo "Copying iOS target template..."
-dest="$HOME/Library/Developer/Xcode/Templates/SpecKit-Templates/Project Templates/SpecKit/iOS Spec Runner.xctemplate"
-mkdir -p "$dest"
-cp "Templates/iOS Spec Runner.xctemplate"/* "$dest"
-echo "Done."
-
-echo "Copying Mac target template..."
-dest="$HOME/Library/Developer/Xcode/Templates/SpecKit-Templates/Project Templates/SpecKit/Mac Spec Runner.xctemplate"
-mkdir -p "$dest"
-cp "Templates/Mac Spec Runner.xctemplate"/* "$dest"
+echo "Copying templates..."
+mkdir -p ~/Library/Developer/Xcode/Templates/
+cp -R Templates/ ~/Library/Developer/Xcode/Templates/
 echo "Done."

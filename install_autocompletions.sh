4e88758b69d475d690808e20d0a30796bc3d0bf5
Steven Degutis <steven.degutis@gmail.com>
2012-09-06 10:52:14 -0500

    removing extraneous copy script

--- install_autocompletions.sh
+++ install_autocompletions.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env bash
 
-echo "Copying templates..."
-mkdir -p ~/Library/Developer/Xcode/Templates/
-cp -R Templates/ ~/Library/Developer/Xcode/Templates/
 echo "Copying code snippets..."
 mkdir -p ~/Library/Developer/Xcode/UserData/CodeSnippets/
 cp -R CodeSnippets/ ~/Library/Developer/Xcode/UserData/CodeSnippets/

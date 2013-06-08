c2a20d87fa1cbead1079f285bb861d351affa885
Nathan Walker <earthworn@gmail.com>
2013-06-07 22:54:45 -0500

    Added spec for float expectations.

---
+++ OCDSpec2Specs/FloatExpectationSpec.m
@@ -0,0 +1,54 @@
+#import <OCDSpec2/OCDSpec2.h>
+
+#import "OCDSFloatExpectation.h"
+#import "OCDSFakeFailureReporter.h"
+
+OCDSpec2Context(FloatExpectationSpec) {
+  
+  __block OCDSFakeFailureReporter *reporter;
+  
+  BeforeEach(^{
+    reporter = [OCDSFakeFailureReporter new];
+  });
+  
+  AfterEach(^{
+    [reporter release];
+  });
+  
+  Describe(@"-toBeWithPrecision", ^{
+    
+    It(@"passes when two floats are equal", ^{
+      [[[OCDSFloatExpectation expectationInFile:"file1" line:2 failureReporter:reporter] withFloat]
+       (1.0) toBe: 1.0 withPrecision: 0.0000001];
+      
+      [ExpectInt(reporter.numberOfFailures) toBe:0];
+    });
+    
+    It(@"passes when two floats are not equal but close enough", ^{
+      [[[OCDSFloatExpectation expectationInFile:"file11" line:22 failureReporter:reporter] withFloat]
+       (1.99999) toBe: 2.0 withPrecision: 0.0001];
+      
+      [ExpectInt(reporter.numberOfFailures) toBe:0];
+    });
+    
+    It(@"fails when two floats are not equal", ^{
+      [[[OCDSFloatExpectation expectationInFile:"file11" line:22 failureReporter:reporter] withFloat]
+       (1.2) toBe: 2.1234 withPrecision: 0.0000001];
+      NSString *report = [reporter findFailureMessageInFile:@"file11"
+                                                     onLine:22];
+      
+      [ExpectObj(report) toBeEqualTo:@"Want 2.1234, got 1.2"];      
+    });
+    
+    It(@"fails when two floats are not equal with very high precision", ^{
+      [[[OCDSFloatExpectation expectationInFile:"file11" line:22 failureReporter:reporter] withFloat]
+       (1.99999) toBe: 2.0 withPrecision: 0.0000001];
+      NSString *report = [reporter findFailureMessageInFile:@"file11"
+                                                     onLine:22];
+      
+      [ExpectObj(report) toBeEqualTo:@"Want 2, got 1.99999"];
+    });
+    
+  });
+  
+}
